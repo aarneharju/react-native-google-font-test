803bbf7b1a9214d40c33355c4ef2df0493b84d04
aarneharju <35223549+aarneharju@users.noreply.github.com>
2023-01-24 23:59:25 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useFonts, Lato_100Thin } from '@expo-google-fonts/lato';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Lato_100Thin,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontFamily: 'Lato_100Thin', fontSize: 40 }}>Lato Thin</Text>
     </View>
   );
 }
